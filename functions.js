9297d197e58071724d3899b4bfb63b16b9623aff
Igor Lopes <97232379+igorlops@users.noreply.github.com>
2022-08-20 00:52:27 -0300

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,16 @@
+$(function(){
+		$('.btn-add').click(function(){
+			var el = '<div class="anotacao-single"><textarea placeholder="Sua nova anotação..."></textarea></div>'
+			$('.container-anotacoes').append(el);
+
+			var textArea = $('.anotacao-single').last().find('textarea');
+
+			var date = new Date();
+
+			var hh = date.getHours();
+			var mn = date.getMinutes();
+
+			var finalTime = hh+":"+mn;
+			textArea.html("Nova anotação em: "+finalTime);
+		})
+	})
